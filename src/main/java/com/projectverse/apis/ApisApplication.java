fef14ea1158d1bcab9b4609751ec783060873d85
Vidushi Chauhan <vidushic0@gmail.com>
2025-02-17 22:43:18 -0500

    added .env file

--- src/main/java/com/projectverse/apis/ApisApplication.java
+++ src/main/java/com/projectverse/apis/ApisApplication.java
@@ -2,12 +2,17 @@ package com.projectverse.apis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import io.github.cdimascio.dotenv.Dotenv;
 @SpringBootApplication
 public class ApisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApisApplication.class, args);
+
+
+		Dotenv dotenv = Dotenv.load();
+		String apiKey = dotenv.get("API_KEY");
+
 	}
 
 }
